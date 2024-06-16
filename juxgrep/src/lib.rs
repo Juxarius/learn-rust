c6774876d17a0d8af2ae37af9d219ce2450b3d51
Juxarius <exdarkrb@gmail.com>
2024-06-16 20:02:32 +0800

    Lesson 2
    Implemented case insensitive search

--- juxgrep/src/lib.rs
+++ juxgrep/src/lib.rs
@@ -1,15 +1,27 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents: String = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+    
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
     Ok(())
 }
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -21,12 +33,37 @@ impl Config {
         // In Rust the ownership of the object will change if you just assign it to another variable. Thus, we need to clone here, and also why we prefer to work with pointers
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+
+        // Basically os.environ["CASE_INSENSITIVE"] with try-catch
+        // In windows use this:
+        // $Env:CASE_INSENSITIVE = "true"
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
-pub fn search(query: &str, contents: &str) -> Vec<&str> {
-    vec![]
+// You have to specify the lifetime of the returned vector by tying it to one of the input parameters. In this case, we are tying it to contents as it is most logical to do so
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 // This is how tests are written for a module in Rust
@@ -43,5 +80,16 @@ Rust:
 safe, fast, productive.
 Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
     }
 }
--- juxgrep/src/main.rs
+++ juxgrep/src/main.rs
@@ -8,13 +8,14 @@ fn main() {
 
     let config: Config = Config::new(&args)
     .unwrap_or_else(|err| {
-        println!("Error: Failed to parse arguments - {}", err);
+        eprintln!("Error: Failed to parse arguments - {}", err);
         process::exit(1);
     });
     println!("Searching for {} in {}", config.query, config.filename);
 
+    // eprintln prints to stderr
     if let Err(e) = juxgrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
